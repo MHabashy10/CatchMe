0fa55891dbb9b4d18b31a5a385e6c28d46dacffd
Mohamed Habashy <mohamed.habshey10@gmail.com>
2017-08-16 23:12:01 +0200

    adding pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,62 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { MyApp } from './app.component';
+
+import { TabsPage } from '../pages/tabs/tabs';
 
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ItemDetailsPage } from '../pages/item-details/item-details';
 import { ListPage } from '../pages/list/list';
+import { MapPage } from '../pages/map/map';
+import { FriendsPage } from '../pages/friends/friends';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+var config = {
+  apiKey: "AIzaSyBRvwabU-PiSF-pT7co5IBW8TARWUa2pTw",
+  authDomain: "popping-heat-2454.firebaseapp.com",
+  databaseURL: "https://popping-heat-2454.firebaseio.com",
+  projectId: "popping-heat-2454",
+  storageBucket: "",
+  messagingSenderId: "485505836289"
+};
 @NgModule({
   declarations: [
     MyApp,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
+    ListPage,
+    TabsPage,
+    MapPage,
+    FriendsPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config, 'my-app'),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
+    ListPage,
+    TabsPage,
+    MapPage,
+    FriendsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
