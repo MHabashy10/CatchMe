d3694c88c3c89288c94f812f8ca9ffc89d9f2cab
Mohamed Habashy <mohamed.habshey10@gmail.com>
2017-08-16 23:11:45 +0200

    add components, tabs integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,35 @@ import { Component, ViewChild } from '@angular/core';
 
 import { Platform, MenuController, Nav } from 'ionic-angular';
 
+import { TabsPage } from '../pages/tabs/tabs';
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ListPage } from '../pages/list/list';
+import { FriendsPage } from '../pages/friends/friends';
+import { MapPage } from '../pages/map/map';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-
+export interface PageInterface {
+  title: string;
+  name: string;
+  component: any;
+  icon: string;
+  logsOut?: boolean;
+  index?: number;
+  tabName?: string;
+  tabComponent?: any;
+}
 @Component({
   templateUrl: 'app.html'
 })
+
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  // make HelloIonicPage the root (or first) page
-  rootPage = HelloIonicPage;
-  pages: Array<{title: string, component: any}>;
+  // make TabsPage the root (or first) page
+  rootPage = TabsPage;
+  pages: PageInterface[];
 
   constructor(
     public platform: Platform,
@@ -29,8 +42,10 @@ export class MyApp {
 
     // set our app's pages
     this.pages = [
-      { title: 'Hello Ionic', component: HelloIonicPage },
-      { title: 'My First List', component: ListPage }
+      { title: 'Hello', name: 'TabsPage', component: TabsPage, tabComponent: HelloIonicPage, index: 0, icon: 'calendar' },
+      { title: 'Map', name: 'TabsPage', component: TabsPage, tabComponent: MapPage, index: 1, icon: 'contacts' },
+      { title: 'Friends', name: 'TabsPage', component: TabsPage, tabComponent: FriendsPage, index: 2, icon: 'contacts' },
+      { title: 'List', name: 'TabsPage', component: TabsPage, tabComponent: ListPage, index: 3, icon: 'contacts' },
     ];
   }
 
@@ -43,10 +58,36 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
+
+
+  openPage(page: PageInterface) {
+    let params = {};
+
+    // the nav component was found using @ViewChild(Nav)
+    // setRoot on the nav to remove previous pages and only have this page
+    // we wouldn't want the back button to show in this scenario
+    if (page.index) {
+      params = { tabIndex: page.index };
+    }
+
+    // If we are already on tabs just change the selected tab
+    // don't setRoot again, this maintains the history stack of the
+    // tabs even if changing them from the menu
+    if (this.nav.getActiveChildNavs() && page.index != undefined) {
+      this.nav.getActiveChildNavs()[0].select(page.index);
+      // Set the root of the nav with params if it's a tab index
+    } else {
+      this.nav.setRoot(page.name, params).catch((err: any) => {
+        console.log(`Didn't set nav root: ${err}`);
+      });
+    }
+
+    if (page.logsOut === true) {
+      // Give the menu time to close before changing to logged out
+      // this.userData.logout();
+    }
+
     // close the menu when clicking a link from the menu
     this.menu.close();
-    // navigate to the new page if it is not the current page
-    this.nav.setRoot(page.component);
   }
 }
