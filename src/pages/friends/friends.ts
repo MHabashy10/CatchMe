8aed5032b77e2464aa217c883664d95e3518f146
Mohamed Habashy <mohamed.habshey10@gmail.com>
2017-08-16 23:09:49 +0200

    friends page

---
+++ src/pages/friends/friends.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import {  NavController, NavParams } from 'ionic-angular';
+
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+import { ItemDetailsPage } from '../item-details/item-details';
+/**
+ * Generated class for the FriendsPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+
+@Component({
+  selector: 'page-friends',
+  templateUrl: 'friends.html',
+})
+export class FriendsPage {
+  items: FirebaseListObservable<any[]>;
+  constructor(public navCtrl: NavController, public navParams: NavParams, afAuth: AngularFireAuth, db: AngularFireDatabase) {
+    this.items = db.list('users');
+  }
+  itemTapped(event, item) {
+    this.navCtrl.push(ItemDetailsPage, {
+      item: item
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad FriendsPage');
+  }
+
+}
