f66a3ef450bf52c7f954be887918a74cf9547cdb
Mohamed Habashy <mohamed.habshey10@gmail.com>
2017-08-19 22:32:47 +0200

    login ionicpage

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,25 +1,53 @@
+
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ProfilePage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-
-
+import { NavController, AlertController, LoadingController, Loading, IonicPage } from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service';
+ 
+@IonicPage()
 @Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html',
+  selector: 'page-login',
+  templateUrl: 'login.html',
 })
-export class ProfilePage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+export class LoginPage {
+  loading: Loading;
+  registerCredentials = { email: '', password: '' };
+ 
+  constructor(private nav: NavController, private auth: AuthService, private alertCtrl: AlertController, private loadingCtrl: LoadingController) { }
+ 
+  public createAccount() {
+    this.nav.push('RegisterPage');
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
+ 
+  public login() {
+    this.showLoading()
+    this.auth.login(this.registerCredentials).subscribe(allowed => {
+      if (allowed) {        
+        this.nav.setRoot('TabsPage');
+      } else {
+        this.showError("Access Denied");
+      }
+    },
+      error => {
+        this.showError(error);
+      });
   }
-
+ 
+  showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...',
+      dismissOnPageChange: true
+    });
+    this.loading.present();
+  }
+ 
+  showError(text) {
+    this.loading.dismiss();
+ 
+    let alert = this.alertCtrl.create({
+      title: 'Fail',
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present(prompt);
+  }
 }
