b734b563626b93b8980558997d3eae6d7b120c00
Mohamed Habashy <mohamed.habshey10@gmail.com>
2017-08-19 22:31:17 +0200

    add authorizaition provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
+
+import { AuthService } from './../providers/auth-service';
+
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { MyApp } from './app.component';
 
-import { TabsPage } from '../pages/tabs/tabs';
-
-import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ItemDetailsPage } from '../pages/item-details/item-details';
 import { ListPage } from '../pages/list/list';
 import { MapPage } from '../pages/map/map';
 import { FriendsPage } from '../pages/friends/friends';
+
 
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -29,16 +31,16 @@ var config = {
 @NgModule({
   declarations: [
     MyApp,
-    HelloIonicPage,
     ItemDetailsPage,
     ListPage,
-    TabsPage,
     MapPage,
     FriendsPage
+    
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
     AngularFireModule.initializeApp(config, 'my-app'),
     AngularFireDatabaseModule,
     AngularFireAuthModule
@@ -46,17 +48,17 @@ var config = {
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HelloIonicPage,
     ItemDetailsPage,
     ListPage,
-    TabsPage,
     MapPage,
     FriendsPage
+    
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    AuthService
   ]
 })
 export class AppModule { }
