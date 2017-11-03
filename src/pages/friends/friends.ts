fd0804f6b292753e1dc0201b260b2386fff6e666
Mohamed Habashy <mohamed.habshey10@gmail.com>
2017-11-03 22:02:55 +0200

    fix new angularfire

--- src/pages/friends/friends.ts
+++ src/pages/friends/friends.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {  NavController, NavParams } from 'ionic-angular';
 
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 
 import { ItemDetailsPage } from '../item-details/item-details';
+
+import { Observable } from 'rxjs/Observable';
 /**
  * Generated class for the FriendsPage page.
  *
@@ -18,9 +20,9 @@ import { ItemDetailsPage } from '../item-details/item-details';
   templateUrl: 'friends.html',
 })
 export class FriendsPage {
-  items: AngularFireList<any[]>;
+  items: Observable<any[]>;
   constructor(public navCtrl: NavController, public navParams: NavParams, afAuth: AngularFireAuth, db: AngularFireDatabase) {
-    this.items = db.list('users');
+    this.items = db.list('users').valueChanges();
   }
   itemTapped(event, item) {
     this.navCtrl.push(ItemDetailsPage, {
