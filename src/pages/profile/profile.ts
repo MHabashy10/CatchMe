33ed571cc01c3ea16f2ef53b26d15d688d14f1e7
Mohamed Habshey <Mohamed.habshey10@gmail.com>
2017-11-05 15:33:55 +0200

    fetch userInfo from auth Serivce

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, IonicPage } from 'ionic-angular';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService, User } from '../../providers/auth-service';
 
 
 /**
@@ -12,54 +13,44 @@ import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @IonicPage()
 @Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html',
+     selector: 'page-profile',
+     templateUrl: 'profile.html',
 })
 
 export class ProfilePage {
 
-  user: ProfileInterface;
-  private profileForm: FormGroup;
-  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
-
-    this.user = {
-      firstName: "Mohamed",
-      lastName: "Habashy",
-      email: "any",
-      phone: 528799333
-    };
-    this.profileForm = this.formBuilder.group({
-      firstName: [this.user.firstName, Validators.required],
-      lastName: [this.user.lastName, Validators.required],
-      email: [this.user.email, Validators.required],
-      phone: [this.user.phone, Validators.required]
-    });
-
-    this.profileForm.disable()
-  }
-  logForm() {
-    console.log(this.profileForm.value);
-    // this.profileForm.disable()
-  }
-
-  toggleForm() {
-    console.log(this.profileForm.value);
-   
-    setTimeout(()=> this.profileForm.enable())
-
-  }
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
+     user: User;
+     private profileForm: FormGroup;
 
 
+     constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, private auth: AuthService) {
+          // get currentUser info
+          this.user = this.auth.getUserInfo();
+          // create profile formBuilder
+          this.profileForm = this.formBuilder.group({
+               firstName: [this.user.firstName, Validators.required],
+               lastName: [this.user.lastName, Validators.required],
+               email: [this.user.email, Validators.required],
+               phone: [this.user.phone, Validators.required]
+          });
+          // by default disable the form
+          this.profileForm.disable()
+     }
+     logForm() {
+          console.log(this.profileForm.value);
+          // this.profileForm.disable()
+     }
 
+     toggleForm() {
 
-  }
+          console.log(this.profileForm.value);
+          // enable the form for editing
+          this.profileForm.enable()
+
+     }
+     ionViewDidLoad() {
+          console.log('ionViewDidLoad ProfilePage');
+     }
 
 }
-export interface ProfileInterface {
-  firstName: string;
-  lastName: string;
-  email: any;
-  phone: number;
-}
+
