07c4d8d9affb8e5595e285fc4279b48cbab29e7c
Mohamed Habashy <mohamed.habshey10@gmail.com>
2017-11-05 02:10:06 +0200

    formGroup cotnroller

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, IonicPage } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+
 
 /**
  * Generated class for the ProfilePage page.
@@ -8,18 +10,56 @@ import { NavController, NavParams } from 'ionic-angular';
  * on Ionic pages and navigation.
  */
 
-
+@IonicPage()
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html',
 })
+
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  user: ProfileInterface;
+  private profileForm: FormGroup;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
+
+    this.user = {
+      firstName: "Mohamed",
+      lastName: "Habashy",
+      email: "any",
+      phone: 528799333
+    };
+    this.profileForm = this.formBuilder.group({
+      firstName: [this.user.firstName, Validators.required],
+      lastName: [this.user.lastName, Validators.required],
+      email: [this.user.email, Validators.required],
+      phone: [this.user.phone, Validators.required]
+    });
+
+    this.profileForm.disable()
+  }
+  logForm() {
+    console.log(this.profileForm.value);
+    // this.profileForm.disable()
   }
 
+  toggleForm() {
+    console.log(this.profileForm.value);
+   
+    this.profileForm.enable()
+    this.profileForm.enable()
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
+
+
+
+
   }
 
+}
+export interface ProfileInterface {
+  firstName: string;
+  lastName: string;
+  email: any;
+  phone: number;
 }
