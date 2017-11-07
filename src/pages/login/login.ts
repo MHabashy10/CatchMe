120c1d150bccb1dfac4851df94d493e06db3a848
Mohamed Habshey <Mohamed.habshey10@gmail.com>
2017-11-07 18:15:14 +0200

    check for now on _id

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -21,7 +21,7 @@ export class LoginPage {
      public login() {
           this.showLoading()
           this.auth.login(this.registerCredentials).subscribe(allowed => {
-               if (allowed.status) {
+               if (allowed._id) {
                     this.nav.setRoot('TabsPage');
                } else {
                     this.showError("Access Denied");
