134ad5bf4164053beb2fc0dea62e14a303f3d036
Mohamed Habashy <mohamed.habshey10@gmail.com>
2017-11-16 00:07:21 +0200

    list ionicPage decorator

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, IonicPage } from 'ionic-angular';
 
 import { ItemDetailsPage } from '../item-details/item-details';
 
+@IonicPage()
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
