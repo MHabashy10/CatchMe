f3e7136de39fd3759059df47ffeca1d2712e2712
Mohamed Habashy <mohamed.habshey10@gmail.com>
2017-11-16 02:04:15 +0200

    AIO components module

---
+++ src/components/components.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ControlMessages } from './control-messages/control-messages';
+@NgModule({
+	declarations: [
+		ControlMessages],
+	imports: [],
+	exports: [
+		ControlMessages]
+})
+export class ComponentsModule {}
